18d7248919169906012248f6d5a9bfa3272da158
martijnvrencken96 <martijn_vrencken@hotmail.com>
2023-05-05 12:44:02 +0200

    car added

---
+++ explanations.js
@@ -0,0 +1,28 @@
+// explanations
+var favColor = 'red';
+var myFavColors = ['red','blue','orange'];
+var numbOfFavColors = 3;
+var hasGotFavColors = true;
+
+var richObject = {
+	firstName: 'martijn',
+	lastName: 'Vrencken',
+	favColors: ['red','blue','orange'],
+	yearsAlive: 26,
+	isMale: true
+
+};
+
+var whatIsMyFavColor = function () {
+	return false;
+
+	return 'blue';
+}
+
+var doSomething = function () {
+	console.log('Do Something')
+}
+
+var area = function (width, heigth) {
+	return width * heigth
+}
